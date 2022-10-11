6b3ef033e3e34287a0216026b078fdecd2930940
user <bxl190001@utdallas.edu>
2022-10-11 16:21:46 -0500

    rewrite Forall_app and Forall_map to not use Forall_ap and Forall_map from library

--- a5/luo.v
+++ a5/luo.v
@@ -12,16 +12,30 @@ Theorem Forall_app:
   forall (A:Type) P (l1 l2:list A),
     Forall P (l1++l2) <-> Forall P l1 /\ Forall P l2.
 Proof.
-  apply Forall_app.
+  intros A P l1 l2.
+  split.
+  - intros H. split.
+  + induction l1; intros. 
+    apply Forall_nil.
+    simpl. apply Forall_cons. apply Forall_inv with (l := l1 ++ l2). apply H.
+    simpl in *. apply IHl1. apply Forall_inv_tail with (a := a). apply H.
+  + induction l1; intros.
+    simpl in *. apply H.
+    simpl in *. apply IHl1. apply Forall_inv_tail with (a := a). apply H.
+  - intros H. destruct H. induction l1; intros; simpl.
+  + apply H0. 
+  + apply Forall_cons with (l := l1 ++ l2). 
+    apply Forall_inv in H. apply H.
+    apply IHl1. apply Forall_inv_tail in H. apply H.
 Qed.
 
 Theorem Forall_map:
   forall (A B:Type) P (f:A->B) l,
     Forall (fun x => P (f x)) l <-> Forall P (map f l).
 Proof.
-  intros A B P f l.
-  rewrite Forall_map.
-  reflexivity.
+  induction l; intros.
+  - simpl. rewrite Forall_nil_iff. rewrite Forall_nil_iff. reflexivity.
+  - simpl in *. rewrite Forall_cons_iff. rewrite IHl. rewrite <- Forall_cons_iff. reflexivity.
 Qed.
 
 Theorem Forall_noadd:
